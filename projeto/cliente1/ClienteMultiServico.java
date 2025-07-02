134950900b67d6083ece12879dee7d61c3ac9ec9
Victor Hugo <142616340+vh-preza@users.noreply.github.com>
2025-07-02 16:57:21 -0300

    switch

--- projeto/cliente1/ClienteMultiServico.java
+++ projeto/cliente1/ClienteMultiServico.java
@@ -12,24 +12,45 @@ public class ClienteMultiServico {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // CPF
-        System.out.print("Digite um CPF (somente numeros): ");
-        String cpf = sc.nextLine();
+        int escolha = 1;
 
-        ValidadorCPFServiceImplService cpfService = new ValidadorCPFServiceImplService();
-        ValidadorCPFServiceImpl cpfPorta = cpfService.getValidadorCPFServiceImplPort();
-        boolean cpfValido = cpfPorta.validarCPF(cpf);
-        System.out.println(" CPF valido? " + cpfValido);
+        while(escolha !=0){
 
-        // Placa
-        System.out.print("Digite uma placa de carro (ex: ABC1D23): ");
-        String placa = sc.nextLine();
+        System.out.println("insira os valor \n 1 - info cpf \n 2 - info placa \n 0 - sair do programa");
 
-        ValidadorPlacaServiceImplService placaService = new ValidadorPlacaServiceImplService();
-        ValidadorPlacaServiceImpl placaPorta = placaService.getValidadorPlacaServiceImplPort();
-        boolean placaValida = placaPorta.validarPlaca(placa);
-        System.out.println(" Placa valida? " + placaValida);
+        escolha = sc.nextInt();
+        sc.nextLine(); 
 
+            switch(escolha){
+                case 1: 
+                    System.out.print("Digite um CPF (somente numeros): ");
+                    String cpf = sc.nextLine();
+
+                    ValidadorCPFServiceImplService cpfService = new ValidadorCPFServiceImplService();
+                    ValidadorCPFServiceImpl cpfPorta = cpfService.getValidadorCPFServiceImplPort();
+                    boolean cpfValido = cpfPorta.validarCPF(cpf);
+                    System.out.println(" CPF valido? " + cpfValido);
+                    break;
+
+                case 2:
+                    System.out.print("Digite uma placa de carro (ex: ABC1D23): ");
+                    String placa = sc.nextLine();
+
+                    ValidadorPlacaServiceImplService placaService = new ValidadorPlacaServiceImplService();
+                    ValidadorPlacaServiceImpl placaPorta = placaService.getValidadorPlacaServiceImplPort();
+                    boolean placaValida = placaPorta.validarPlaca(placa);
+                    System.out.println(" Placa valida? " + placaValida);
+                    break;
+
+                case 0:
+                    System.out.println("Programa encerrado.");
+                    break;
+
+                default:
+                    System.out.println("Opcao invalida.");
+            }
+        
+        }
         sc.close();
     }
 }
